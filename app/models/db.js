e2ec966dcc0f5d9af232dc7f81efd6a89b869b13
Patrick Kiely <kiely.patrick@gmail.com>
2017-11-04 22:52:23 +0000

    API files added

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const User = require('../models/user');
+
 mongoose.Promise = global.Promise;
 
 let dbURI = 'mongodb://localhost/mytweet';
@@ -14,6 +16,17 @@ mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
+  if (process.env.NODE_ENV != 'production') {
+    var seeder = require('mongoose-seeder');
+    const data = require('./data.json');
+    const User = require('./user');
+    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
+      console.log(dbData);
+    }).catch(err => {
+      console.log(error);
+    });
+  }
 });
 
 mongoose.connection.on('error', function (err) {
--- index.js
+++ index.js
@@ -37,6 +37,8 @@ server.register([require('inert'), require('vision'), require('hapi-auth-cookie'
   });
 
 server.route(require('./routes'));
+server.route(require('./routesapi'));
+
 
 server.start((err) => {
   if (err) {
